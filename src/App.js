2af535cf13a4ab788aecaef51edf2365c46d07fa
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-06-26 16:35:30 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import  { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function App() {
   return (
     <div className="App pricing">
+      <div className='container'>
       <div className='row'>
         <div className='col-lg-4'>
           <div className='card mb-5 mb-lg-0'>
             <div className='card-body'>
-              <h5 className='card-title'>Free</h5>
-              <h6 className='card-price'>$0<span className='period'>/month</span></h6>
-              <ul className='fa'>
-                <li><span className='fa'><i className='fas'></i></span>Single User</li>
-                <li><span className='fa'><i className='fas'></i></span>5GB Storage</li>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
-                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
-                <li><span className='text-muted'><i className='fas'></i></span>Unlimited Private Projects</li>
-                <li><span className='text-muted'><i className='fas'></i></span>Dedicated Phone Support</li>
-                <li><span className='text-muted'><i className='fas'></i></span>Free Subdomain</li>
-                <li><span className='text-muted'><i className='fas'></i></span>Monthly Status Reports</li>
+              <h5 className='card-title text-muted text-uppercase text-center'>Free</h5>
+              <h6 className='card-price text-center'>$0<span className='period'>/month</span></h6>
+              <ul className='fa-ul'>
+                <li><span className='fa-li'><i className='fas'><FontAwesomeIcon  icon={faCheck}/> </i></span>Single User</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>5GB Storage</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited Public Projects</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Community Access</li>
+                <li className='text-muted'><span className='fa-li'><i className='icon'><FontAwesomeIcon icon={faXmarkCircle} /></i></span>Unlimited Private Projects</li>
+                <li className='text-muted'><span className='fa-li'><i className='icon'><FontAwesomeIcon icon={faXmarkCircle} /></i></span>Dedicated Phone Support</li>
+                <li className='text-muted'><span className='fa-li'><i className='icon'><FontAwesomeIcon icon={faXmarkCircle} /></i></span>Free Subdomain</li>
+                <li className='text-muted'><span className='fa-li'><i className='icon'><FontAwesomeIcon icon={faXmarkCircle} /></i></span>Monthly Status Reports</li>
               </ul>
               <div className='d-grid'>
-                <a href='_blank' className='btn'>Button</a>
+                <a href='_blank' className='btn btn-primary text-uppercase'>Button</a>
               </div>
             </div>
           </div>
@@ -29,20 +36,20 @@ function App() {
         <div className='col-lg-4'>
           <div className='card mb-5 mb-lg-0'>
             <div className='card-body'>
-              <h5 className='card-title'>Plus</h5>
-              <h6 className='card-price'>$9<span className='period'>/month</span></h6>
-              <ul className='fa'>
-                <li><span className='fa'><i className='fas'></i></span>5 Users</li>
-                <li><span className='fa'><i className='fas'></i></span>50GB Storage</li>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
-                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited Private Projects</li>
-                <li><span className='fa'><i className='fas'></i></span>Dedicated Phone Support</li>
-                <li><span className='fa'><i className='fas'></i></span>Free Subdomain</li>
-                <li><span className='fa'><i className='fas'></i></span>Monthly Status Reports</li>
+              <h5 className='card-title text-muted text-uppercase text-center'>Plus</h5>
+              <h6 className='card-price text-center'>$9<span className='period'>/month</span></h6>
+              <ul className='fa-ul'>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>5 Users</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>50GB Storage</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited Public Projects</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Community Access</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Dedicated Phone Support</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited Private Projects</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Free Subdomain</li>
+                <li className='text-muted'><span className='fa-li'><i className='icon'><FontAwesomeIcon icon={faXmarkCircle} /></i></span>Monthly Status Reports</li>
               </ul>
               <div className='d-grid'>
-                <a href='_blank' className='btn'>Button</a>
+                <a href='_blank' className='btn btn-primary text-uppercase'>Button</a>
               </div>
             </div>
           </div>
@@ -51,25 +58,26 @@ function App() {
         <div className='col-lg-4'>
           <div className='card mb-5 mb-lg-0'>
             <div className='card-body'>
-              <h5 className='card-title'>Pro</h5>
-              <h6 className='card-price'>$49<span className='period'>/month</span></h6>
-              <ul className='fa'>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited User</li>
-                <li><span className='fa'><i className='fas'></i></span>150GB Storage</li>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
-                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
-                <li><span className='fa'><i className='fas'></i></span>Unlimited Private Projects</li>
-                <li><span className='fa'><i className='fas'></i></span>Dedicated Phone Support</li>
-                <li><span className='fa'><i className='fas'></i></span>Free Subdomain</li>
-                <li><span className='fa'><i className='fas'></i></span>Monthly Status Reports</li>
+              <h5 className='card-title text-muted text-uppercase text-center'>Pro</h5>
+              <h6 className='card-price text-center'>$49<span className='period'>/month</span></h6>
+              <ul className='fa-ul'>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited User</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>150GB Storage</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited Public Projects</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Community Access</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Unlimited Private Projects</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Dedicated Phone Support</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Free Subdomain</li>
+                <li><span className='fa-li'><i className='icon'><FontAwesomeIcon  icon={faCheck}/></i></span>Monthly Status Reports</li>
               </ul>
               <div className='d-grid'>
-                <a href='_blank' className='btn'>Button</a>
+                <a href='_blank' className='btn btn-primary text-uppercase'>Button</a>
               </div>
             </div>
           </div>
         </div>
       </div>
+     </div>
     </div>
   );
 }
