e13bf09895e23542c59ffa0e10cf2f4475930cb1
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-06-26 08:48:26 +0530

    adding cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App pricing">
+      <div className='row'>
+        <div className='col-lg-4'>
+          <div className='card mb-5 mb-lg-0'>
+            <div className='card-body'>
+              <h5 className='card-title'>Free</h5>
+              <h6 className='card-price'>$0<span className='period'>/month</span></h6>
+              <ul className='fa'>
+                <li><span className='fa'><i className='fas'></i></span>Single User</li>
+                <li><span className='fa'><i className='fas'></i></span>5GB Storage</li>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
+                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
+                <li><span className='text-muted'><i className='fas'></i></span>Unlimited Private Projects</li>
+                <li><span className='text-muted'><i className='fas'></i></span>Dedicated Phone Support</li>
+                <li><span className='text-muted'><i className='fas'></i></span>Free Subdomain</li>
+                <li><span className='text-muted'><i className='fas'></i></span>Monthly Status Reports</li>
+              </ul>
+              <div className='d-grid'>
+                <a href='_blank' className='btn'>Button</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-lg-4'>
+          <div className='card mb-5 mb-lg-0'>
+            <div className='card-body'>
+              <h5 className='card-title'>Plus</h5>
+              <h6 className='card-price'>$9<span className='period'>/month</span></h6>
+              <ul className='fa'>
+                <li><span className='fa'><i className='fas'></i></span>5 Users</li>
+                <li><span className='fa'><i className='fas'></i></span>50GB Storage</li>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
+                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited Private Projects</li>
+                <li><span className='fa'><i className='fas'></i></span>Dedicated Phone Support</li>
+                <li><span className='fa'><i className='fas'></i></span>Free Subdomain</li>
+                <li><span className='fa'><i className='fas'></i></span>Monthly Status Reports</li>
+              </ul>
+              <div className='d-grid'>
+                <a href='_blank' className='btn'>Button</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-lg-4'>
+          <div className='card mb-5 mb-lg-0'>
+            <div className='card-body'>
+              <h5 className='card-title'>Pro</h5>
+              <h6 className='card-price'>$49<span className='period'>/month</span></h6>
+              <ul className='fa'>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited User</li>
+                <li><span className='fa'><i className='fas'></i></span>150GB Storage</li>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited Public Projects</li>
+                <li><span className='fa'><i className='fas'></i></span>Community Access</li>
+                <li><span className='fa'><i className='fas'></i></span>Unlimited Private Projects</li>
+                <li><span className='fa'><i className='fas'></i></span>Dedicated Phone Support</li>
+                <li><span className='fa'><i className='fas'></i></span>Free Subdomain</li>
+                <li><span className='fa'><i className='fas'></i></span>Monthly Status Reports</li>
+              </ul>
+              <div className='d-grid'>
+                <a href='_blank' className='btn'>Button</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
